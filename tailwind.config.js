38c25841279304388b6b4e8f8566b01ad9f961d1
SharonNavarro <shanmowrk@gmail.com>
2023-02-16 15:28:23 -0500

    add: angular's icon with Font Awesome and configuration of screen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,15 @@ module.exports = {
         danger: colors.red
       }
     },
+    container: {
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1024px',
+        '2xl': '1536px'
+      }
+    }
   },
   plugins: [
     require('@tailwindcss/forms')
