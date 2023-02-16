d5f7625dea2060820e7d3bfb40c8040a30d245e8
SharonNavarro <shanmowrk@gmail.com>
2023-02-16 14:12:45 -0500

    feat: multiple overlays

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
 
   isOpen: boolean = false;
+  isOpenBody: boolean = false;
 
   constructor() { }
 
