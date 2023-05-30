1f80efea30f21cbda71a2d993ab20b4b8e3fefae
SharonNavarro <shanmowrk@gmail.com>
2023-05-30 00:48:34 -0500

    add: shared's module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,44 +2,35 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
-import { OverlayModule } from '@angular/cdk/overlay';
 import { CdkAccordionModule } from '@angular/cdk/accordion';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { DialogModule } from '@angular/cdk/dialog';
 import { ScrollingModule } from '@angular/cdk/scrolling';
 import { CdkTableModule } from '@angular/cdk/table';
 
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
-import { BtnComponent } from './components/btn/btn.component';
 import { BoardsComponent } from './pages/boards/boards.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { BoardComponent } from './pages/board/board.component';
-import { TodoDialogComponent } from './components/todo-dialog/todo-dialog.component';
 import { ScrollComponent } from './pages/scroll/scroll.component';
 import { TableComponent } from './pages/table/table.component';
+import { SharedModule} from './modules/shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    BtnComponent,
     BoardsComponent,
-    NavbarComponent,
     BoardComponent,
-    TodoDialogComponent,
     ScrollComponent,
     TableComponent,
   ],
   imports: [
+    SharedModule,
     DragDropModule,
     BrowserModule,
-    OverlayModule,
     AppRoutingModule,
-    FontAwesomeModule,
     CdkAccordionModule,
     DialogModule,
     HttpClientModule,
---
+++ src/app/modules/shared/shared.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BtnComponent } from "./components/btn/btn.component";
+import { TodoDialogComponent } from "./components/todo-dialog/todo-dialog.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { OverlayModule } from '@angular/cdk/overlay';
+
+@NgModule({
+  declarations: [
+    BtnComponent,
+    TodoDialogComponent,
+    NavbarComponent
+  ],
+  imports: [
+    CommonModule,
+    FontAwesomeModule,
+    OverlayModule
+  ]
+})
+export class SharedModule { }
