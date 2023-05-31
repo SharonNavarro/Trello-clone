0c0b66e898f9e2b34a612c068bb9f882f289dd3a
SharonNavarro <shanmowrk@gmail.com>
2023-05-31 00:30:12 -0500

    add: background, footer, header and login-form components in auth module

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,14 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import { LoginComponent } from './pages/login/login.component';
+import { SharedModule } from '../shared/shared.module';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+
+import { BackgroundComponent } from './components/background/background.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+
 import { AuthRoutingModule } from './auth-routing.module';
+
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    LoginFormComponent,
+    BackgroundComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    SharedModule,
+    ReactiveFormsModule,
+    FontAwesomeModule
   ]
 })
 export class AuthModule { }
