d49f14216ca12a58b5f164d22328a688cbfc2a96
SharonNavarro <shanmowrk@gmail.com>
2023-06-11 19:17:50 -0500

    add: new components and pages in auth's module

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './pages/login/login.component';
+import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { RecoveryComponent } from './pages/recovery/recovery.component';
 
 const routes: Routes = [
   {
@@ -13,6 +16,21 @@ const routes: Routes = [
     path: 'login',
     component: LoginComponent,
     title: 'Login'
+  },
+  {
+    path: 'forgot-password',
+    component: ForgotPasswordComponent,
+    title: 'Forgot Password'
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    title: 'Register'
+  },
+  {
+    path: 'recovery',
+    component: RecoveryComponent,
+    title: 'Recovery'
   }
 ];
 
--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -4,12 +4,19 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { LoginComponent } from './pages/login/login.component';
-import { SharedModule } from '../shared/shared.module';
-import { LoginFormComponent } from './components/login-form/login-form.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { RecoveryComponent } from './pages/recovery/recovery.component';
+import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
 
+import { SharedModule } from '../shared/shared.module';
+
+import { LoginFormComponent } from './components/login-form/login-form.component';
 import { BackgroundComponent } from './components/background/background.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ForgotPasswordFormComponent } from './components/forgot-password-form/forgot-password-form.component';
+import { RecoveryFormComponent } from './components/recovery-form/recovery-form.component';
+import { RegisterFormComponent } from './components/register-form/register-form.component';
 
 import { AuthRoutingModule } from './auth-routing.module';
 
@@ -19,7 +26,13 @@ import { AuthRoutingModule } from './auth-routing.module';
     LoginFormComponent,
     BackgroundComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    RegisterComponent,
+    RecoveryComponent,
+    ForgotPasswordComponent,
+    ForgotPasswordFormComponent,
+    RecoveryFormComponent,
+    RegisterFormComponent
   ],
   imports: [
     CommonModule,
