b934b7d1c8429bb35da2c47148c92f2f6f3db5fa
SharonNavarro <shanmowrk@gmail.com>
2023-06-12 00:11:35 -0500

    delete: shared and dialog module form app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { DialogModule } from '@angular/cdk/dialog';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule} from './modules/shared/shared.module';
 
 @NgModule({
   declarations: [
