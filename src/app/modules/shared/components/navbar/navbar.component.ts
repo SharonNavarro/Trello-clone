ad71ecb5b2d74e23250bec73c4763d4034195294
SharonNavarro <shanmowrk@gmail.com>
2023-06-12 00:16:55 -0500

    refactor: edit navbar name component from shared module

--- src/app/modules/shared/components/navbar/navbar.component.ts
+++ src/app/modules/shared/components/navbar/navbar.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { faBell, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
-  selector: 'app-navbar',
+  selector: 'app-navbar-shared',
   templateUrl: './navbar.component.html'
 })
 export class NavbarComponent implements OnInit {
