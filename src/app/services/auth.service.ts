0d787eab8278821ce55d026dc6d581dea65b69c6
SharonNavarro <shanmowrk@gmail.com>
2023-06-14 17:03:16 -0500

    feat: optimize the registration flow, automatic login after registration

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { environment } from '@environments/environment';
-import { Observable } from 'rxjs';
+import { Observable, switchMap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -28,6 +28,13 @@ export class AuthService {
       email,
       password
     })
+  }
+
+  registerAndLogin(name: string, email: string, password: string) {
+    return this.register(name, email, password)
+    .pipe(
+      switchMap(() => this.login(email, password))
+    );
   }
 
   isAvailable(email:string) {
