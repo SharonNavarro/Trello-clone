d451e77e818875644c6845c708a825ef37f9997c
SharonNavarro <shanmowrk@gmail.com>
2023-06-16 01:52:14 -0500

    feat: auth guard from module

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,7 +2,12 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { environment } from '@environments/environment';
-import { Observable, switchMap } from 'rxjs';
+
+import { Observable, switchMap, tap } from 'rxjs';
+
+import { TokenService } from './token.service';
+
+import { ResponseLogin } from '@models/auth.model'
 
 @Injectable({
   providedIn: 'root'
@@ -12,14 +17,22 @@ export class AuthService {
   apiUrl = environment.API_URL;
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private tokenService: TokenService
   ) { }
 
   login(email: string, password: string): Observable<any> {
-    return this.http.post(`${this.apiUrl}/auth/login`, {
+    return this.http.post<ResponseLogin>(`${this.apiUrl}/auth/login`, {
       email,
       password
     })
+      .pipe(
+        tap({
+          next: (response) => {
+            this.tokenService.saveToken(response.access_token);
+          }
+        })
+      )
   }
 
   register(name: string, email: string, password: string): Observable<any> {
@@ -32,22 +45,22 @@ export class AuthService {
 
   registerAndLogin(name: string, email: string, password: string) {
     return this.register(name, email, password)
-    .pipe(
-      switchMap(() => this.login(email, password))
-    );
+      .pipe(
+        switchMap(() => this.login(email, password))
+      );
   }
 
-  isAvailable(email:string) {
-    return this.http.post<{isAvailable: boolean}>(`${this.apiUrl}/auth/is-available`, {
+  isAvailable(email: string) {
+    return this.http.post<{ isAvailable: boolean }>(`${this.apiUrl}/auth/is-available`, {
       email
     })
   }
 
-  recovery(email:string) {
-    return this.http.post<{isAvailable: boolean}>(`${this.apiUrl}/auth/recovery`, {email});
+  recovery(email: string) {
+    return this.http.post<{ isAvailable: boolean }>(`${this.apiUrl}/auth/recovery`, { email });
   }
 
   changePassword(token: string, newPassword: string) {
-    return this.http.post<{isAvailable: boolean}>(`${this.apiUrl}/auth/change-password`, {token, newPassword});
+    return this.http.post<{ isAvailable: boolean }>(`${this.apiUrl}/auth/change-password`, { token, newPassword });
   }
 }
