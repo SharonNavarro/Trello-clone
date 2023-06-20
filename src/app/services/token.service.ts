280ec45b1fbc0cfb8cdebfd8d5748328e7012284
SharonNavarro <shanmowrk@gmail.com>
2023-06-19 23:22:33 -0500

    add: ngx-cookie-service dependencie and in the service

--- src/app/services/token.service.ts
+++ src/app/services/token.service.ts
@@ -1,20 +1,30 @@
 import { Injectable } from '@angular/core';
+import { CookieService } from "ngx-cookie-service";
+import { getCookie, setCookie, removeCookie } from "typescript-cookie";
 
 @Injectable()
 export class TokenService {
 
-  constructor() { }
+  constructor(
+    private _cookies: CookieService
+  ) { }
 
   saveToken(token: string) {
-    localStorage.setItem('token', token);
+    // localStorage.setItem('token', token);
+    // setCookie('token', token, {expires: 365, path: '/'});
+    this._cookies.set("token", token, 1, '/');
   }
 
   getToken() {
-    return localStorage.getItem('token');
+    // return localStorage.getItem('token');
+    // const token = getCookie("token");
+    return this._cookies.get("token");
   }
 
   removeToken() {
-    localStorage.removeItem('token');
+    // localStorage.removeItem('token');
+    // removeCookie("token");
+    this._cookies.delete("token");
   }
 
 }
